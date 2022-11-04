980f7d2ca90539ee853460a1a1b335d35dfb7414
Yuma Tsuchida <65499273+hagerondev@users.noreply.github.com>
2022-11-05 00:11:37 +0900

    Update arm.sh

--- arm.sh
+++ arm.sh
@@ -23,7 +23,7 @@ apt update
 // docker をインストール
 apt install -y docker-ce
 
-apt -y install python3-pip unzip
+apt -y install python3-pip unzip jq
 
 apt install language-pack-ja -y
 update-locale LANG=ja_JP.UTF-8
