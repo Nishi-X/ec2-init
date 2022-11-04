3cc9927c7d7bbe2b00910aa04b6b3a6aa58ad51e
Yuma Tsuchida <65499273+hagerondev@users.noreply.github.com>
2022-11-04 14:21:38 +0900

    remove sudo

--- arm.sh
+++ arm.sh
@@ -5,31 +5,31 @@ $nrconf{kernelhints} = '0';
 $nrconf{restart} = 'a';
 EOF
 
-sudo apt update
-sudo apt -y upgrade
+apt update
+apt -y upgrade
 
 // 必要パッケージをインストール
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 // Dockerの公式GPG keyを追加
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 // repository を追加
-sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" --yes
+add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" --yes
 
 // パッケージのアップデート
-sudo apt update
+apt update
 
 // docker をインストール
-sudo apt install -y docker-ce
+apt install -y docker-ce
 
-sudo apt -y install python3-pip
+apt -y install python3-pip
 
-sudo apt install language-pack-ja -y
-sudo update-locale LANG=ja_JP.UTF-8
-sudo ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
-sudo dpkg-reconfigure --frontend noninteractive tzdata
+apt install language-pack-ja -y
+update-locale LANG=ja_JP.UTF-8
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
 
 rm setup.sh
 
-sudo reboot
+reboot
